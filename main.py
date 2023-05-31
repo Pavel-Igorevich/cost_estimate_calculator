3fbd79c53a86849ff534baa24e9d1b4a36164b7f
Павел Смирнов <smirnov@transtelesoft.com>
2023-05-31 17:03:40 +0400

    сделана страница с баннером

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def test():
 @app.route('/canvas', methods=['GET', 'POST'])
 def canvas():
     form = CanvasForm()
+    return_data = None
     if form.validate_on_submit():
         data_canvsas = DATA['Холст']['data']
         price_material = data_canvsas['Материал'][form.material.data]['Себестоимость']
@@ -50,71 +51,82 @@ def canvas():
             'main_price': main_price,
             'main_sale_price': main_sale_price
         }
-        return render_template('canvas.html', form=form, products=PRODUCTS, data=return_data)
-    else:
-        return render_template('canvas.html', form=form, products=PRODUCTS)
+
+    return render_template('canvas.html', form=form, products=PRODUCTS, data=return_data)
     
     
 @app.route('/banner', methods=['GET', 'POST'])
 def banner():
     form = BannerForm()
     if form.validate_on_submit():
-        data_banner = DATA['Баннер']['data']
-        price_material = data_banner['Материал'][form.material.data][form.print_quality.data]['Себестоимость']
-        sale_price_material = data_banner['Материал'][form.material.data][form.print_quality.data]['Продажа']
-
-        if form.welding_step.data != 'Недоступно':
-            welding_step = form.welding_step.data
-            price_proccessing = data_banner['Обработка'][form.processing.data][welding_step]['Себестоимость']
-            sale_price_proccessing = data_banner['Обработка'][form.processing.data][welding_step]['Продажа']
-        else:
-            welding_step = False
-            price_proccessing = data_banner['Обработка'][form.processing.data]['Себестоимость']
-            sale_price_proccessing = data_banner['Обработка'][form.processing.data]['Продажа']
-            
-        length = form.length.data
-        width = form.width.data
-        if length < 500:
-            length = 500
-        if width < 500:
-            width = 500
-        square = (float(length) / 1000) * (float(width) / 1000)
-        perimeter = (float(length) / 1000 + float(width) / 1000) * 2
+        for _ in form.data.items():
+            print(_)
+        # data_banner = DATA['Баннер']['data']
+        # price_material = data_banner['Материал'][form.material.data][form.print_quality.data]['Себестоимость']
+        # sale_price_material = data_banner['Материал'][form.material.data][form.print_quality.data]['Продажа']
+        #
+        # if form.welding_step.data != 'Недоступно':
+        #     welding_step = form.welding_step.data
+        #     price_proccessing = data_banner['Обработка'][form.processing.data][welding_step]['Себестоимость']
+        #     sale_price_proccessing = data_banner['Обработка'][form.processing.data][welding_step]['Продажа']
+        # else:
+        #     welding_step = False
+        #     price_proccessing = data_banner['Обработка'][form.processing.data]['Себестоимость']
+        #     sale_price_proccessing = data_banner['Обработка'][form.processing.data]['Продажа']
+        #
+        # length = form.length.data
+        # width = form.width.data
+        # if length < 500:
+        #     length = 500
+        # if width < 500:
+        #     width = 500
+        # square = (float(length) / 1000) * (float(width) / 1000)
+        # perimeter = (float(length) / 1000 + float(width) / 1000) * 2
+        #
+        # price_processing_side = 0
+        # processing_sides = []
+        # # todo сделать вывод ошибок с возможностью их изменения
+        # if data_banner['Обработка'][form.processing.data].get('Сторона'):
+        #     for key_side, val_side in form.data.items():
+        #         if 'side' in key_side and val_side:
+        #             processing_sides.append(form[key_side].label.text)
+        #             if key_side in ['left_side', 'right_side']:
+        #                 price_processing_side += (float(length) / 1000) * sale_price_proccessing
+        #             elif key_side in ['top_side', 'down_side']:
+        #                 price_processing_side += (float(width) / 1000) * sale_price_proccessing
+        #             elif key_side == 'around_side':
+        #                 price_processing_side += perimeter * sale_price_proccessing
+        #             elif key_side == 'corner_side':
+        #                 price_processing_side += sale_price_proccessing
+        #     if not processing_sides:
+        #         error_message = 'Не заданы стороны обработки!'
+        #         return render_template('banner.html', form=form, products=PRODUCTS, error=error_message)
+        # #     todo -----------------------------
+        #
+        #
+        # # todo получить формулу посчета
+        # main_price = 1000.00
+        # main_sale_price = 2000.00
         
-        price_processing_side = 0
-        processing_sides = []
-        # todo сделать вывод ошибок с возможностью их изменения
-        if data_banner['Обработка'][form.processing.data].get('Сторона'):
-            for key_side, val_side in form.data.items():
-                if 'side' in key_side and val_side:
-                    processing_sides.append(form[key_side].label.text)
-                    if key_side in ['left_side', 'right_side']:
-                        price_processing_side += (float(length) / 1000) * sale_price_proccessing
-                    elif key_side in ['top_side', 'down_side']:
-                        price_processing_side += (float(width) / 1000) * sale_price_proccessing
-                    elif key_side == 'around_side':
-                        price_processing_side += perimeter * sale_price_proccessing
-                    elif key_side == 'corner_side':
-                        price_processing_side += sale_price_proccessing
-            if not processing_sides:
-                error_message = 'Не заданы стороны обработки!'
-                return render_template('banner.html', form=form, products=PRODUCTS, error=error_message)
-        #     todo -----------------------------
-        
-
-        # todo получить формулу посчета
-        main_price = 1000.00
-        main_sale_price = 2000.00
-        
+        # return_data = {
+        #     'material': [form.material.data, price_material, sale_price_material],
+        #     'processing': [form.processing.data, price_proccessing, sale_price_proccessing],
+        #     'welding_step': welding_step,
+        #     'processing_side': ['test', price_processing_side],
+        #     'size': square,
+        #     'quantity': form.quantity.data,
+        #     'main_price': main_price,
+        #     'main_sale_price': main_sale_price
+        # }
         return_data = {
-            'material': [form.material.data, price_material, sale_price_material],
-            'processing': [form.processing.data, price_proccessing, sale_price_proccessing],
-            'welding_step': welding_step,
-            'processing_side': ['test', price_processing_side],
-            'size': square,
-            'quantity': form.quantity.data,
-            'main_price': main_price,
-            'main_sale_price': main_sale_price
+            'material': ['form.material.data', 'price_material', 'sale_price_material'],
+            'processing': ['form.processing.data', 'price_proccessing', 'sale_price_proccessing'],
+            'welding_step': 'welding_step',
+            'processing_side': ['test', 'price_processing_side'],
+            'size': 'square',
+            'quantity': 'form.quantity.data',
+            'main_price': 'main_price',
+            'main_sale_price': 'main_sale_price'
         }
         return render_template('banner.html', form=BannerForm(), products=PRODUCTS, data=return_data)
     else:
